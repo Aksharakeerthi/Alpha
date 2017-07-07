f93b686261833ff7cc99522c66bde0ea65288398
Aksharakeerthi <akshara2223@gmail.com>
2017-07-07 15:13:37 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,20 @@
+# Alpha
+import java.io.*;
+imort java.util.*;
+public class Alpha{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+char k;
+System.out.println("Enter the character");
+k=sc.nextChar();
+int length=characters.length();
+for(int i=0;i<length;i++);
+{
+char character = characters.charAt(i);
+if(Character.isLetter(character)){
+System.out.println("Character is alphabet");
+}
+else
+System.out.println("Character is not a alphabet");
+}
+}
